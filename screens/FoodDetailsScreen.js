9ccb854319080b87157b1e5cdb99fd3a327421a4
anselme <amotchojr@gmail.com>
2023-07-31 14:54:40 -0400

    commit

--- screens/FoodDetailsScreen.js
+++ screens/FoodDetailsScreen.js
@@ -1,116 +1,58 @@
-// FoodDetailsScreen.js
-import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ChevronLeftIcon } from 'react-native-heroicons/solid';
 import { HeartIcon, MinusIcon, PlusIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from '@react-navigation/native';
 import * as Animatable from 'react-native-animatable';
-import styles from './FoodDetailsScreenStyles';
+import FoodDetailsStyles from './FoodDetailsStyles';
 
 export default function FoodDetailsScreen(props) {
   let item = props.route.params;
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
+    <View style={FoodDetailsStyles.container}>
       <Image
+        style={{ borderBottomLeftRadius: 50, borderBottomRightRadius: 50 }}
         source={require('../assets/images/background.png')}
-        style={styles.backgroundImage}
         blurRadius={40}
+        style={FoodDetailsStyles.backgroundImage}
       />
       <SafeAreaView>
-        <View style={styles.topButtonsContainer}>
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            style={styles.backButton}
-          >
+        <View style={FoodDetailsStyles.header}>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={FoodDetailsStyles.headerButton}>
             <ChevronLeftIcon size={23} stroke={50} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.heartButton}>
+          <TouchableOpacity style={FoodDetailsStyles.headerButton}>
             <HeartIcon size={23} color="black" />
           </TouchableOpacity>
         </View>
-        <View style={styles.foodImageContainer}>
-          <Image source={item.image} style={styles.foodImage} />
-          <Text style={styles.foodNameText}>{item.name}</Text>
+        <View style={FoodDetailsStyles.centerContent}>
+          <Image style={FoodDetailsStyles.foodImage} source={item.image} />
+          <Text style={FoodDetailsStyles.foodName}>{item.name}</Text>
         </View>
-        <View style={styles.quantityContainer}>
-          <View style={styles.quantityButton}>
-            <MinusIcon size={20} strokeWidth={1.8} color="black" />
-          </View>
-          <Text style={styles.quantityText}>1</Text>
-          <View style={styles.quantityButton}>
-            <PlusIcon size={20} strokeWidth={1.8} color="black" />
-          </View>
-        </View>
-        <View style={styles.infoContainer}>
-          <Animatable.View
-            delay={180}
-            animation="slideInDown"
-            style={styles.infoItem}
-          >
-            <Image
-              source={require('../assets/icons/calories.png')}
-              style={styles.infoIcon}
-            />
-            <Text style={styles.infoText}>130 cal</Text>
-          </Animatable.View>
-          <Animatable.View
-            delay={280}
-            animation="slideInDown"
-            style={styles.infoItem}
-          >
-            <Image
-              source={require('../assets/icons/clock.png')}
-              style={styles.infoIcon}
-            />
-            <Text style={styles.infoText}>15-20 min</Text>
-          </Animatable.View>
-          <Animatable.View
-            delay={380}
-            animation="slideInDown"
-            style={styles.infoItem}
-          >
-            <Image
-              source={require('../assets/icons/chat.png')}
-              style={styles.infoIcon}
-            />
-            <Text style={styles.infoText}>4.6 vote</Text>
-          </Animatable.View>
-          <Animatable.View
-            delay={480}
-            animation="slideInDown"
-            style={styles.infoItem}
-          >
-            <Image
-              source={require('../assets/icons/weight.png')}
-              style={styles.infoIcon}
-            />
-            <Text style={styles.infoText}>350 g</Text>
-          </Animatable.View>
-        </View>
-        <View style={styles.descriptionContainer}>
-          <Animatable.Text
-            animation="slideInUp"
-            style={styles.descriptionTitle}
-          >
-            Description
-          </Animatable.Text>
-          <Animatable.Text delay={100} animation="slideInUp" style={styles.descriptionText}>
-            {item.desc}
-          </Animatable.Text>
-        </View>
-        {/* add to cart button */}
-        <View style={styles.priceContainer}>
-          <Animatable.Text delay={100} animation="slideInLeft" style={styles.priceText}>
-            ${item.price}
-          </Animatable.Text>
-          <Animatable.View delay={100} animation="slideInRight">
-            <TouchableOpacity style={styles.addToCartButton}>
-              <Text style={styles.addToCartText}>Add to Cart</Text>
+        <View style={FoodDetailsStyles.quantityContainer}>
+          <View style={FoodDetailsStyles.quantityButtons}>
+            <TouchableOpacity style={FoodDetailsStyles.quantityButton}>
+              <MinusIcon size={20} strokeWidth={1.8} color="black" />
             </TouchableOpacity>
-          </Animatable.View>
+            <Text style={FoodDetailsStyles.quantityText}>1</Text>
+            <TouchableOpacity style={FoodDetailsStyles.quantityButton}>
+              <PlusIcon size={20} strokeWidth={1.8} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={FoodDetailsStyles.infoContainer}>
+          {/* Animatable Views */}
+        </View>
+        <View style={FoodDetailsStyles.descriptionContainer}>
+          {/* Animatable Texts */}
+        </View>
+        {/* Add to cart button */}
+        <View style={FoodDetailsStyles.addToCartContainer}>
+          {/* Animatable Text */}
+          {/* Animatable View */}
         </View>
       </SafeAreaView>
     </View>
--- styles/foodDetailsScreenStyles.js
+++ styles/foodDetailsScreenStyles.js
@@ -1,57 +1,83 @@
-// foodDetailsScreenStyles.js
-
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-    image: {
-        borderBottomLeftRadius: 50,
-        borderBottomRightRadius: 50,
-        height: 96,
-        width: '100%',
-        position: 'absolute',
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginHorizontal: 4,
-        alignItems: 'center',
-    },
-    backButton: {
-        backgroundColor: 'white',
-        borderRadius: 50,
-        padding: 3,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.2,
-        shadowRadius: 1.41,
-        elevation: 2,
-    },
-    heartButton: {
-        backgroundColor: 'white',
-        borderRadius: 50,
-        padding: 3,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.2,
-        shadowRadius: 1.41,
-        elevation: 2,
-    },
-    absolute: {
-        position: 'absolute',
-        top: 10,
-        left: 10,
-    },
-    childElement: {
-        marginRight: 10, // Adjust this value as needed
-    },
-    text: {
-        letterSpacing: 2, // Adjust this value as needed
-    },
-    // Define other styles here...
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  backgroundImage: {
+    height: 96,
+    width: '100%',
+    position: 'absolute',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+    alignItems: 'center',
+  },
+  headerButton: {
+    backgroundColor: 'white',
+    borderRadius: 25,
+    padding: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  centerContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  foodImage: {
+    height: 48,
+    width: 48,
+  },
+  foodName: {
+    fontSize: 30,
+    color: 'white',
+  },
+  quantityContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 6,
+  },
+  quantityButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    borderRadius: 50,
+    paddingHorizontal: 12,
+  },
+  quantityButton: {
+    borderRadius: 50,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: 'gray',
+    padding: 8,
+  },
+  quantityText: {
+    fontSize: 20,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+    height: 20,
+    overflow: 'hidden',
+  },
+  descriptionContainer: {
+    marginHorizontal: 16,
+    marginTop: 6,
+    height: 60,
+    justifyContent: 'space-between',
+  },
+  addToCartContainer: {
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
-
-export default styles;
